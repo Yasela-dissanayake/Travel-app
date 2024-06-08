27dc1785abf753905f49af123e7bfee51e898397
Yasela-dissanayake <yasela2014@gmail.com>
2024-06-08 11:56:27 +0530

    feat: navbar completed

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,15 +1,44 @@
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className="flexBetween max-container padding-container z-30 py-5">
+    <div className="flex justify-between items-center max-container padding-container z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex"></ul>
+      <ul className="hidden h-full gap-12 lg:flex ">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flex items-center justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flex hidden">
+        <Button
+          title="Login"
+          type="button"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </div>
   );
 };
