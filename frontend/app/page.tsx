fdf7435959655b3ec476540fcf28b6e5ee312b73
Yasela-dissanayake <yasela2014@gmail.com>
2024-06-12 20:25:57 +0530

    WIP

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,6 @@ import Features from "@/components/Features";
 import GetInTouch from "@/components/GetInTouch";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
